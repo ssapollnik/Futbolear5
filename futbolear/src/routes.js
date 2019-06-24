9de040d7119496c3321123bbc45ab0ab88d0ec19
Max Szuchman <maxszuchman@MacBook-Air-de-Max.local>
2019-06-24 10:49:43 -0300

    agrego detalles dentro de routes

--- futbolear/src/routes.js
+++ futbolear/src/routes.js
@@ -1,9 +1,11 @@
 import homePartidos from './components/home.vue';
 import newMatch from './components/newMatch.vue';
+import detalles from './components/detalles.vue';
 
 const routes = [
     { path: '/', component: homePartidos },
-    { path: '/newMatch', component: newMatch }
+    { path: '/newMatch', component: newMatch },
+    { path: '/detalles/:id', component: detalles }
 ];
 
 export default routes;
