3b825daff80d305e54051f920533fa3b024f6264
SEBASTIAN SAPOLL¥IK <36754986@ort>
2019-06-26 19:26:04 -0300

    arreglo branch

--- README.md
+++
@@ -1,2 +0,0 @@
-# Futbolear5
-Futbolear para NT2
--- hello-world/src/routes.js
+++
@@ -1,9 +0,0 @@
-import HelloWorld from './components/HelloWorld.vue';
-import newMatch from './components/newMatch.vue';
-
-const routes = [
-    { path: '/', component: HelloWorld },
-    { path: '/newMatch', component: newMatch }
-];
-
-export default routes;
